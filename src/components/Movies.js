e8042ffcd8fde5a47fe1d28ab63c3def054bf75c
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-08-31 16:02:15 -0600

    this displays all of the movies on the movies page

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,10 +2,23 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
+  const movieobjs = movies.map((movie) => {
+    const genres = movie.genres.map((genre) => <li>{genre}</li>);
+    return (
+      <div>
+        <h2>Name: {movie.title}</h2>
+        <p>Time: {movie.time}</p>
+        <ul>
+          {genres}
+        </ul>
+      </div>
+    );
+  });
+
   return (
     <div>
       <h1>Movies Page</h1>
-      <div>{/* div for each movie*/}</div>
+      {movieobjs}
     </div>
   );
 }
