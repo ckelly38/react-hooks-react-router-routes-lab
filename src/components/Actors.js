a542a8930748460ad89b3b9f10395f25b8d97aee
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-08-31 16:30:10 -0600

    this displays the actors and the movies they were in the design is also similar to the other two components

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,24 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>Actors</div>;
+  const actorobjs = actors.map((actor) => {
+    const amviesobjs = actor.movies.map((mv) => <li>{mv}</li>);
+    return (
+      <div>
+        <h2>Name: {actor.name}</h2>
+        <ul>
+          {amviesobjs}
+        </ul>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actorobjs}
+    </div>
+  );
 }
 
 export default Actors;
