9310bb82f7628125b0a2bc89fa7c6ab13648a0e9
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-08-31 15:47:42 -0600

    this sets up the NavBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,16 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <div>
+      <ul>
+        <li><NavLink to="/movies">Movies</NavLink></li>
+        <li><NavLink to="/directors">Directors</NavLink></li>
+        <li><NavLink to="/actors">Actors</NavLink></li>
+        <li><NavLink exact to="/">Home</NavLink></li>
+      </ul>
+    </div>
+  );
 }
 
 export default NavBar;
