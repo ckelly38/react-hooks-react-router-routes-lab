bf888152c918d9ef4f5661f804d1ef42ea461601
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-08-31 16:18:59 -0600

    this displays all of the information about the directors and the design is very similar to the movies page

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,22 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>Directors</div>;
+  const mdirobjs = directors.map((director) => {
+    const moviesobjs = director.movies.map((movie) => <li>{movie}</li>)
+    return (
+      <div>
+        <h2>Name: {director.name}</h2>
+        <ul>{moviesobjs}</ul>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {mdirobjs}
+    </div>
+  );
 }
 
 export default Directors;
